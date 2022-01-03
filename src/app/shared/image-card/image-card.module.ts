9b5ad3a2310083050283b2663fc32a7390ceb1f9
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2022-01-03 17:41:43 -0600

    Agregado de modulos en la carpeta shared

---
+++ src/app/shared/image-card/image-card.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageCardComponent } from './image-card.component';
+
+@NgModule({
+    declarations: [
+        ImageCardComponent,
+    ],
+    imports: [
+        CommonModule
+    ], 
+    exports: [
+        ImageCardComponent,
+    ]
+})
+export class ImageCardModule { }
---
+++ src/app/shared/secondary-card/secondary-card.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SecondaryCardComponent } from './secondary-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    SecondaryCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SecondaryCardModule { }
